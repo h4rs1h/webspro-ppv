eef750bfbf0523181e3509b45d838e07229dd307
Harsih Rianto <harsih@mediaprimajaringan.com>
2026-06-08 14:15:03 +0700

    fix: perbaiki path gambar headline di beranda/home.blade.php
    - Ganti backslash Windows (\asset\images\) menjadi helper Laravel asset()
    - Perbaikan agar kompatibel di semua environment (Linux/Windows)

--- resources/views/beranda/home.blade.php
+++ resources/views/beranda/home.blade.php
@@ -1,9 +1,8 @@
 @extends('layouts.app')
 
 @section('content')
-{{--  carousel  --}}
 <div class="container">
-<div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
+  <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
@@ -11,16 +10,13 @@
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active" style="max-height: 600px; overflow:hidden ">
-        <img src="\asset\images\headline1.jpg" class="bd-placeholder-img" width="100%" height="100%" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
-
+        <img src="{{ asset('asset/images/headline1.jpg') }}" class="bd-placeholder-img" width="100%" height="100%" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
       </div>
       <div class="carousel-item" style="max-height: 600px; overflow:hidden ">
-        <img src="\asset\images\headline2.jpg" class="bd-placeholder-img" width="100%" height="100%" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
+        <img src="{{ asset('asset/images/headline2.jpg') }}" class="bd-placeholder-img" width="100%" height="100%" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
       </div>
       <div class="carousel-item" style="max-height: 600px; overflow:hidden ">
-        <img src="\asset\images\headline3.png" class="bd-placeholder-img" width="100%" height="100%" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
-
-
+        <img src="{{ asset('asset/images/headline3.png') }}" class="bd-placeholder-img" width="100%" height="100%" aria-hidden="true" preserveAspectRatio="xMidYMid slice" focusable="false">
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#myCarousel" data-bs-slide="prev">
